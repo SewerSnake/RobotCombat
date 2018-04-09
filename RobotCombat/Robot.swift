0b3f4c653b5d3a179ac94f3713b19aa2ad496bf7
SewerSnake <ericgroseclos@hotmail.com>
2018-04-09 11:38:35 +0200

    Created a class called "Robot". It has a name, hit points (hp) and an attack power. It can take damage. When the hp hits zero, it's considered to be destroyed.

---
+++ RobotCombat/Robot.swift
@@ -0,0 +1,56 @@
+//
+//  Robot.swift
+//  RobotCombat
+//
+//  Created by Eric Groseclos on 2018-04-09.
+//  Copyright © 2018 Eric Groseclos. All rights reserved.
+//
+
+import Foundation
+
+class Robot {
+    
+    private var name: String
+    private var hp: Int
+    private var attack: Int
+    
+    // Instantiates the three properties.
+    init(name: String, hp: Int, attack: Int) {
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+    }
+    
+    func getName() -> String {
+        return self.name
+    }
+    
+    func getHp() -> Int {
+        return self.hp
+    }
+    
+    func getAttack() -> Int {
+        return self.attack
+    }
+    
+    // Reduces the robot's hp
+    // by the damage taken.
+    // Ensures that it cannot
+    // go below zero.
+    func takeDamage(damage: Int) {
+        self.hp -= damage
+        
+        if self.hp < 0 {
+            self.hp = 0
+        }
+    }
+    
+    // If the robot's hp is
+    // zero, it's considered to
+    // be destroyed. True is then
+    // returned.
+    func isDestroyed() -> Bool {
+        return self.hp == 0 ? true : false
+    }
+    
+}
